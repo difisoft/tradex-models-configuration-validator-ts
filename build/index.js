70556f56c6c8eaa507b4e3827d81384ed7b518a7
schema-generator <developer@techx.vn>
2020-02-18 02:58:30 +0000

    schema genarator by buildNumber: 233

--- build/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const holidayListRequest_1 = require("./holidayListRequest");
-exports.holidayListRequestSchema = holidayListRequest_1.holidayListRequestSchema;
-exports.holidayListRequestValidator = holidayListRequest_1.holidayListRequestValidator;
-const holidayListResponse_1 = require("./holidayListResponse");
-exports.holidayListResponseSchema = holidayListResponse_1.holidayListResponseSchema;
-exports.holidayListResponseValidator = holidayListResponse_1.holidayListResponseValidator;
-//# sourceMappingURL=index.js.map
